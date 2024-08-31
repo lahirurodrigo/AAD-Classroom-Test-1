cbbeb34bb5e06354fcbacff56e047ca38e99f4bc
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-08-31 10:53:07 +0530

    3rd commit

--- src/main/java/lk/ijse/beans/ChickenRoll.java
+++ src/main/java/lk/ijse/beans/ChickenRoll.java
@@ -1,8 +1,10 @@
 package lk.ijse.beans;
 
 import lk.ijse.contract.BakeryItem;
+import lk.ijse.customannotation.Latest;
 import org.springframework.stereotype.Component;
 
 @Component
+@Latest
 public class ChickenRoll implements BakeryItem {
 }
--- src/main/java/lk/ijse/user/Mahesh.java
+++ src/main/java/lk/ijse/user/Mahesh.java
@@ -1,17 +1,18 @@
 package lk.ijse.user;
 
 import lk.ijse.contract.BakeryItem;
+import lk.ijse.customannotation.Latest;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
-@Qualifier("Third")
 public class Mahesh {
 
     private BakeryItem bakeryItem;
 
+    @Qualifier("Third")
+    @Latest
     public void lovesFood(BakeryItem bakeryItem) {
         this.bakeryItem = bakeryItem;
     }
